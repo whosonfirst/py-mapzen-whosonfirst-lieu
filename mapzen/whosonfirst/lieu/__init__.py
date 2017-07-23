5de737ce7b0055fbfb119db6cc932bedbad5f4f0
thisisaaronland <whosonfirst@mapzen.com>
2017-07-23 14:16:26 +0000

    make it go; bump to 0.0.11

--- mapzen/whosonfirst/lieu/__init__.py
+++ mapzen/whosonfirst/lieu/__init__.py
@@ -13,6 +13,41 @@ def parse_feature_id(f):
     except Exception, e:
         return None, None, e
 
+def to_string(f):
+
+    src, id, err = parse_feature_id(f)
+
+    if err != None:
+        logging.warning(err)
+        return ""
+
+    props = f["properties"]
+    geom = f["geometry"]
+    coords = geom["coordinates"]
+
+    name = props.get("name", None)
+
+    if not name:
+        name = props.get("wof:name")
+
+    housenumber = props["addr:housenumber"]
+    street = props["addr:street"]
+
+    addr_full = props.get("addr:full", None)
+
+    label = [
+        "[%s] %s" % (src, id),
+        name,
+        "%s %s" % (housenumber, street),
+        "%s" % ",".join(map(str, coords)),
+    ]
+
+    if addr_full:
+        label.append(" (%s)" % addr_full)
+
+    label = "\t".join(label)
+    return label
+
 def crawl(parts, **kwargs):
 
     for path in parts:
