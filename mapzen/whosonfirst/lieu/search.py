a774b32ba8a5c896438b19f9d824b559f10a0ed4
thisisaaronland <whosonfirst@mapzen.com>
2017-11-15 23:55:08 +0000

    rewrite the index class to store both records and rollups

--- mapzen/whosonfirst/lieu/search.py
+++ mapzen/whosonfirst/lieu/search.py
@@ -1,4 +1,7 @@
 import mapzen.whosonfirst.elasticsearch
+import hashlib
+import copy
+import json
 
 class index(mapzen.whosonfirst.elasticsearch.index):
 
@@ -6,36 +9,77 @@ class index(mapzen.whosonfirst.elasticsearch.index):
 
         mapzen.whosonfirst.elasticsearch.index.__init__(self, **kwargs)
 
-        self.index = 'lieu'
-        self.doctype = 'lieu'
-        
-    def prepare_report(self, report):
-
-        body = self.prepare_json(report)
-
-        return {
-            'index': self.index,
-            'doc_type': self.doctype,
-            'body': body
-        }
-
-    # https://stackoverflow.com/questions/20288770/how-to-use-bulk-api-to-store-the-keywords-in-es-by-using-python
-
     def prepare_report_bulk(self, report):
-       
-        body = self.prepare_json(report)
 
-        return {
-            # '_id': id,
-            '_index': self.index,
-            '_type': self.doctype,
-            '_source': body
-        }
+        rows = []
 
-    def prepare_json(self, report):
+        str_report = json.dumps(report)
 
-        data = copy.deepcopy(report)
-        return data
+        hash = hashlib.md5()
+        hash.update(str_report)
+        
+        hash_report = hash.hexdigest()
+
+        candidates = [
+            report["object"]
+        ]
+
+        for o in report.get("same_as", []):
+            candidates.append(o["object"])
+
+        for o in report.get("possibly_same_as", []):
+            candidates.append(o["object"])
+        
+        for o in candidates:
+
+            source = copy.deepcopy(o)
+            source["properties"]["lieu:hash"] = hash_report
+
+            row = {
+                "_id": o["properties"]["lieu:guid"],
+                "_index": "lieu",
+                "_type": "record",
+                "_source": source
+            }
+
+            rows.append(row)
+
+        # https://github.com/openvenues/lieu#output-format
+
+        is_dupe = report["is_dupe"]
+        principal = report["object"]
+        
+        principal_id = principal["properties"]["lieu:guid"]
+
+        for rel in ("same_as", "possibly_same_as"):
+
+            for o in report.get(rel, []):
+
+                other = o["object"]
+                other_id = other["properties"]["lieu:guid"]
+                
+                source = copy.deepcopy(o)
+                del(source["object"])
+
+                source["lieu:principal"] = principal_id
+                source["lieu:other"] = other_id
+                source["lieu:rel"] = rel
+                source["lieu:hash"] = hash_report
+                source["is_dupe"] = is_dupe
+            
+                row = {
+                    # "_id": ""			# just let ES do this
+                    "_index": "lieu",
+                    "_type": "rollup",
+                    "_source": source
+                }
+
+                rows.append(row)
+
+        # will this work?
+
+        for row in rows:
+            yield row
 
 class search(mapzen.whosonfirst.elasticsearch.search):
 
